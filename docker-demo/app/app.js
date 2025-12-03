994d2fda7e57ed57402ac5f2d0a376aee99efe9c
root <root@ip-172-31-17-133.ec2.internal>
2025-12-03 13:51:18 +0000

    This will be used in pipeline creation

---
+++ docker-demo/app/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Dockerized Node App!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
